e9ed0844990f60fb58f646f690b8ba146f6126a3
Michael <mickroy412@gmail.com>
2018-11-29 16:19:17 -0500

    Jeu jenga fonctionelle

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ## To play as a human: 
 ```(play-jenga 'human)```
+
+Pour le moment, à part si la tour tombe, il n'y a pas de condition de fin. Il arrive un moment ou la seule chose que l'utilisateur/modèle peut faire c'est de prendre une pièce qui va faire tomber la tour.
--- jenga.lisp
+++ jenga.lisp
@@ -8,76 +8,104 @@
 ; ex: ((T T T) (T NIL T) (NIL T NIL) (T T T) ...)
 (defvar *jenga_state* nil)             
 
+; Keeps a reference to the buttons
+(defvar *jenga_buttons* nil)   
+
 ; The number of total rows
-(defvar *nb_rows* 18)
+(defvar *nb_blocks* 54)
 
 ; The index of the top row
-(defvar *top_row* (- *nb_rows* 1))
+(defvar *top_row* (- (/ *nb_blocks* 3) 1))
+
+; Determines on which column the next removed block will be placed
+(defvar *next_col_place* 0)
+
+; The y position for the bottom row
+(defvar *bottom_y* 850)
+
+; The y position for the bottom row
+(defvar *left_x* 50)
+
+; The y position for the bottom row
+(defvar *right_x* 110)
+
+; The width of the face of a jenga block
+(defvar *block_width* 15)
+
+; The width of the window
+(defvar *window_width* 210)
+
+; The height of the window
+(defvar *window_height* 900)
 
 ; Function that builds the display
 (defun build-display ()
     (setf *experiment-window* (open-exp-window "Jenga"
                                              :visible *visible*
-                                             :width 800
-                                             :height 600))
+                                             :width *window_width*
+                                             :height *window_height*
+                                             :x 200
+                                             :y 25))
   
     (allow-event-manager *experiment-window*)
+)
+
+; Sets the RESET button on the display
+(defun build-reset-button ()
+    ; Reset button
+    (add-button-to-exp-window :x (+ *block_width* (- *right_x* *left_x*)) :y 10 :height 15 :width 50 :text "RESET" :action (lambda (button) (reset)))
 )
 
 ; Clear the display and displays the jenga game
 (defun display-jenga () 
     (setq *jenga_blocks_side_button* (make-array 18))
-
+    
     (clear-exp-window) 
 
-    ; Reset button
-    (add-button-to-exp-window :x 350 :y 20 :height 35 :width 50 :text "RESET" :action (lambda (button) (reset)))
+    (build-reset-button)
 
     ; Side labels
-    (add-text-to-exp-window :text "Side 1" :x 310 :y 140)
-    (add-text-to-exp-window :text "Side 2" :x 410 :y 140)
+    (add-text-to-exp-window :text "Side 1" :x (+ *left_x* 0) :y (+  *bottom_y* 20))
+    (add-text-to-exp-window :text "Side 2" :x (+ *right_x* 0) :y (+  *bottom_y* 20))
 
     ; For each rows, add the buttons reprensenting blocks
-    (loop for row from 0 to 17
-        do (let ((r row) (x (if (equal (mod row 2) 0) 300 400))) 
+    (loop for row from 0 to *top_row*
+        do (let ((r row) (x (if (equal (mod row 2) 0) *left_x* *right_x*))) 
             ; Side of the block on opposite side
-            (add-button-to-exp-window :x (if (equal (mod r 2) 0) 400 300) :y (- 500 (* r 20)) :height 20 :width 60 :text "" )
+            
 
             ; The three blocks we can remove
-            (add-button-to-exp-window :x x :y (- 500 (* r 20)) :height 20 :width 20 :text "" :color (if (equal r 17) 'gray 'blue) :action (if (equal r 17) nil (lambda (button) (block-removed button r 0))))  
-            (add-button-to-exp-window :x (+ x 20) :y (- 500 (* r 20)) :height 20 :width 20 :text "" :color (if (equal r 17) 'gray 'blue) :action (if (equal r 17) nil (lambda (button) (block-removed button r 1))))  
-            (add-button-to-exp-window :x (+ x 40) :y (- 500 (* r 20)) :height 20 :width 20 :text "" :color (if (equal r 17) 'gray 'blue) :action (if (equal r 17) nil (lambda (button) (block-removed button r 2))))         
+            (put-block-at-position r 0)
+            (put-block-at-position r 1)
+            (put-block-at-position r 2)
         )
     )
 )
 
 ; Function that shows tower fall display
 (defun display-fall ()
-    (setf *experiment-window* (open-exp-window "Jenga"
-                                             :visible *visible*
-                                              :width 800
-                                             :height 600))
     ; Clear displays
     (clear-exp-window) 
     
-    ; Reset button
-    (add-button-to-exp-window :x 350 :y 20 :height 35 :width 50 :text "RESET" :action (lambda (button) (reset)))
+    (build-reset-button)
 
     ; Tower fell label
-    (add-text-to-exp-window :text "Tower fell!" :x 350 :y 200)
+    (add-text-to-exp-window :text "Tower fell!" :x (- *right_x* *left_x*) :y 100)
 
     ; Puts random blocks in a pile to look cool
     (loop for row from 0 to 54
-        do (add-button-to-exp-window :x (+ 200 (random 300)) :y (+ 450 (random 100)) :height 20 :width (+ 20 (* 20 (* (random 2) 3))) :text "" )
+        do (add-button-to-exp-window :x (random *window_width*) :y (- *bottom_y* (random 100)) :height *block_width* :width (+ *block_width* (* *block_width* (* (random 2) 3))) :text "" )
     )
-          
 )
 
 ; Resets the game
 (defun reset ()
+    ; Initialize the variables for a new game
     (setf *tower-fell* nil)
-    (setq *jenga_state* (make-array '(18 3) :initial-element t))
-    (setf *top_row* (- *nb_rows* 1))
+    (setq *jenga_state* (make-array '(54 3) :initial-element t))
+    (setq *jenga_buttons* (make-array '(54 3) :initial-element nil))
+    (setf *top_row* (- (/ *nb_blocks* 3) 1))
+    (setf *next_col_place* 0)
 
     (display-jenga)
 )
@@ -86,16 +114,19 @@
 (defun check-row-ok (row) 
     (if (and (not (equal row *top_row*)) 
                 (or (and 
+                    ; [ ] _ _ cause falling
                     (equal (aref *jenga_state* row 0) t)
                     (equal (aref *jenga_state* row 1) nil)
                     (equal (aref *jenga_state* row 2) nil)
                 )
-                (and 
+                (and
+                    ; _ _ [ ] cause falling
                     (equal (aref *jenga_state* row 0) nil)
                     (equal (aref *jenga_state* row 1) nil)
                     (equal (aref *jenga_state* row 2) t)
                 )
                 (and 
+                    ; _ _ _ cause falling
                     (equal (aref *jenga_state* row 0) nil)
                     (equal (aref *jenga_state* row 1) nil)
                     (equal (aref *jenga_state* row 2) nil)
@@ -106,6 +137,59 @@
     )   
 )
 
+; Puts a block at the position in parameters
+(defun put-block-at-position (row col)
+    ; Adds the block on the other side if it's the first block to be added on that row
+    (if (not (aref *jenga_buttons* row col))
+        (add-button-to-exp-window :x (if (equal (mod row 2) 0) *right_x* *left_x*) :y (- *bottom_y* (* row *block_width*)) :height *block_width* :width (* *block_width* 3) :text "" )
+    )
+
+    ; Adds the block and keep a reference of it in *jenga_buttons*
+    (setf 
+        (aref *jenga_buttons* row col) 
+        (add-button-to-exp-window 
+            :x      (+ (if (equal (mod row 2) 0) *left_x* *right_x*) (* *block_width* col)) 
+            :y      (- *bottom_y* (* row *block_width*)) 
+            :height *block_width* 
+            :width  *block_width* 
+            :text   ""  
+            :color  (if (>= row *top_row*) 'gray 'blue) 
+            :action (if (>= row *top_row*) nil (lambda (button) (block-removed button row col)))
+        )
+    )
+)
+
+; Put a the block removed on top of the tower
+(defun put-block-on-top ()
+    (let ((row (+ *top_row* 1)) (x (if (equal (mod (+ *top_row* 1) 2) 0) *left_x* *right_x*)))
+        ; Put the block on the top
+        (put-block-at-position row *next_col_place*)
+        
+        ; If it was the last block to be put on the top, we update the row so that its blocks
+        ; are now able to be removed and also increments the index of the top row
+        (if (equal *next_col_place* 2)
+            (let ((row_below *top_row*))
+                (incf *top_row*)
+                (setf *next_col_place* 0)
+
+                ; Remove the grey blocks from the row below
+                (remove-items-from-exp-window (aref *jenga_buttons* row_below 0))
+                (remove-items-from-exp-window (aref *jenga_buttons* row_below 1))
+                (remove-items-from-exp-window (aref *jenga_buttons* row_below 2))
+
+                ; Replace them with new blocks (the conditions in this function will make them blue because
+                ; they are no longer on the top)
+                (put-block-at-position row_below 0)
+                (put-block-at-position row_below 1)
+                (put-block-at-position row_below 2)
+
+            )
+            ; If its not the last block to be added, simply increment the column index for the next block to be added
+            (incf *next_col_place*) 
+        )
+    )
+)
+
 ; Event happening when the user or model clicks on a block to remove it
 (defun block-removed (button row col)
 
@@ -114,6 +198,7 @@
 
     ; Updates the internal state of jenga (set nil for the block)
     (setf (aref *jenga_state* row col) nil)
+    (setf (aref *jenga_buttons* row col) nil)
 
     ; Test if the new state of the row causes tower to fall
     (if (not (check-row-ok row))
@@ -122,6 +207,8 @@
             (setf *tower-fall* t)                
             (display-fall)
         )
+        ; If it doesn't cause the tower to fall, put the block back on top of the tower
+        (put-block-on-top)
     )
 )
 
@@ -132,12 +219,13 @@
     (reset)
 
     (if (eq who 'human)
-        (progn (print *jenga_state*)
+        ; If it's a human playing, wait for human action
         (wait-for-human)
+        ; If it's the model playing, runs the model for 60 seconds
+        (progn
+            (proc-display :clear t)
+            (run 60 :real-time *visible*))
     )
-    (progn
-        (proc-display :clear t)
-        (run 60 :real-time *visible*)))
 )
 
 ; Utility function to wait for user interaction
